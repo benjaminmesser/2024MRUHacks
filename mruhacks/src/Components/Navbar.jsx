008358b1dff08861fcc47600ce402191c0f6d0dd
bryan <bryan00s689@gmail.com>
2024-10-05 11:48:55 -0600

    Navbar set to top

--- mruhacks/src/Components/Navbar.jsx
+++ mruhacks/src/Components/Navbar.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Navbar = () => {
     return (
         <div>
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
-  <div className="container-fluid">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top w-100">
+            <div className="container-fluid">
     <a className="navbar-brand" href="#">Navbar</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
