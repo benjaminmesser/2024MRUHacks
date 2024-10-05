29a62df45b030d67ad9350fd8e6f0981f03ad24c
John Lowen David <johnlowendavid@yahoo.ca>
2024-10-05 16:36:25 -0600

    fixed the height of the grids

--- mruhacks/src/Components/Main.jsx
+++ mruhacks/src/Components/Main.jsx
@@ -1,37 +1,30 @@
-import React, { useState } from 'react';
-import './Main.css'; // Assuming you'll use an external CSS file
+import React from 'react';
 import QuestManager from './QuestManager';
 import CalendarComponent from './Calender';
 import Rewards from './Rewards';
 
 const Main = () => {
-  const [selectedDate, setSelectedDate] = useState(new Date().toISOString().split('T')[0]); // Default to today's date
-
-  const handleDateChange = (date) => {
-    setSelectedDate(date.toISOString().split('T')[0]); // Update the selected date
-  };
-
-  return (
-    <div className="container-fluid">
-      <div className="row">
-        <div className="col-md-3">
-          <div className="p-3 border bg-light">
-            <QuestManager selectedDate={selectedDate} />
-          </div>
+    return (
+        <div className="container-fluid vh-100 d-flex flex-column" style={{ paddingTop: '50px' }}>
+            <div className="row flex-grow-1"> {/* Use g-0 to remove the gutter */}
+                <div className="col-md-3 d-flex">
+                    <div className="p-3 border bg-light w-100">
+                        <QuestManager />
+                    </div>
+                </div>
+                <div className="col-md-6 d-flex">
+                    <div className="p-3 border bg-light w-100">
+                        <CalendarComponent />
+                    </div>
+                </div>
+                <div className="col-md-3 d-flex">
+                    <div className="p-3 border bg-light w-100">
+                        <Rewards />
+                    </div>
+                </div>
+            </div>
         </div>
-        <div className="col-md-6">
-          <div className="p-3 border bg-light">
-            <CalendarComponent onDateChange={handleDateChange} />
-          </div>
-        </div>
-        <div className="col-md-3">
-          <div className="p-3 border bg-light">
-            <Rewards />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Main;
