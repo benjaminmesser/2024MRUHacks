89d301ee12fca11db24830dc435c1d54ccc83d55
bryan <bryan00s689@gmail.com>
2024-10-05 14:09:35 -0600

    Worked on layout

--- mruhacks/src/Components/Main.jsx
+++ mruhacks/src/Components/Main.jsx
@@ -2,34 +2,30 @@ import React from 'react';
 import './Main.css'; // Assuming you'll use an external CSS file
 import TaskManager from './TaskManager';
 import CalendarComponent from './Calender';
+import Rewards from './Rewards';
 
 const Main = () => {
-  return (
-    <div className="container">
-      {/* Tasks Section */}
-      <div className="task-container">
-        <TaskManager/> {/* Importing your existing Tasks component */}
-      </div>
-
-      {/* Calendar Section */}
-      <div className="calendar-container">
-        <CalendarComponent /> {/* Importing your existing Calendar component */}
-      </div>
-
-      {/* Rewards Section (Future Section) */}
-      <div className="rewards-container">
-        {/* Future rewards section goes here */}
-      </div>
-
-    {/* Bootstrap Card Section */}
-    <div className="card" style={{ width: '50rem' }}>
-      <div className="card-body">
-        <h5 className="card-title">Card Title</h5>
-        <CalendarComponent />
-      </div>
+return (
+    <div className="container-fluid">
+        <div className="row">
+            <div className="col-md-3">
+                <div className="p-3 border bg-light">
+                    <TaskManager />
+                </div>
+            </div>
+            <div className="col-md-6">
+                <div className="p-3 border bg-light">
+                    <CalendarComponent />
+                </div>
+            </div>
+            <div className="col-md-3">
+                <div className="p-3 border bg-light">
+                    <Rewards />
+                </div>
+            </div>
+        </div>
     </div>
-    </div>
-  );
+);
 };
 
 export default Main;
