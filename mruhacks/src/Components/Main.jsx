e9fe2876bf0ce00a12d79ada87c2da04624386c3
Jawne3 <johnlowendavid@yahoo.ca>
2024-10-06 04:42:22 -0600

    fixing bootstrap on main and css on calender

--- mruhacks/src/Components/Main.jsx
+++ mruhacks/src/Components/Main.jsx
@@ -1,36 +1,36 @@
 import React, { useState } from 'react';
-import './Main.css'; // Assuming you'll use an external CSS file
 import QuestManager from './QuestManager';
 import CalendarComponent from './Calender';
 import Rewards from './Rewards';
 
 const Main = (props) => {
-  //props = props.rewardData, props.questData
-  const [selectedDate, setSelectedDate] = useState(new Date().toISOString().split('T')[0]); // Default to today's date
+  const [selectedDate, setSelectedDate] = useState(new Date().toISOString().split('T')[0]);
 
   const handleDateChange = (date) => {
-    setSelectedDate(date.toISOString().split('T')[0]); // Update the selected date
+    setSelectedDate(date.toISOString().split('T')[0]);
   };
 
   return (
     <div className="container-fluid vh-100 d-flex flex-column" style={{ backgroundColor: 'grey', paddingTop: '50px' }}>
-      <div className="row flex-grow-1">
+      <div className="row flex-grow-1 mt-4">
         {/* QuestManager column */}
-        <div className="col-md-3 d-flex">
-          <div className="p-3 border bg-light w-100">
-            <QuestManager selectedDate={selectedDate} questData={props.questData}/>
+        <div className="col-md-3 d-flex mb-4"> {/* Added mb-4 for margin-bottom */}
+          <div className="p-3 w-100" style={{ backgroundColor: 'lightgrey' }}>
+            <QuestManager selectedDate={selectedDate} questData={props.questData} />
           </div>
         </div>
+  
         {/* CalendarComponent column */}
-        <div className="col-md-6 d-flex">
-          <div className="p-3 border bg-light w-100">
+        <div className="col-md-6 d-flex mb-4"> {/* Added mb-4 for margin-bottom */}
+          <div className="p-3 w-100" style={{ backgroundColor: 'lightgrey' }}>
             <CalendarComponent onDateChange={handleDateChange} />
           </div>
         </div>
+  
         {/* Rewards column */}
-        <div className="col-md-3 d-flex">
-          <div className="p-3 border bg-light w-100">
-            <Rewards rewardData={props.rewardData}/>
+        <div className="col-md-3 d-flex mb-4"> {/* Added mb-4 for margin-bottom */}
+          <div className="p-3 w-100" style={{ backgroundColor: 'lightgrey' }}>
+            <Rewards rewardData={props.rewardData} />
           </div>
         </div>
       </div>
