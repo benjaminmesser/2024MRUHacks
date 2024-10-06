248b460cd87e7b7c41708cda499e4b1dcec75319
Justin Nunez <rayjustinn@gmail.com>
2024-10-06 03:01:40 -0600

    added images
    dwadawfaf

--- mruhacks/src/Components/RewardItem.jsx
+++ mruhacks/src/Components/RewardItem.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import avatar from '../assets/avatar.jpg'; // Import the image
+import {avatar , controller, movie, show } from '../assets/'; // Import the image
 import './RewardItem.css';
 
-const RewardItem = ({ description, title, money }) => {
+const RewardItem = ({ description, title, money, image }) => {
   return (
     <div className="card" style={{ width: '100%', maxWidth: '12rem', textAlign: 'center', margin: '0.5rem', position: 'relative' }}>
       <div className="card-body" style={{ paddingBottom: '30px' }}> {/* Add padding to create space for the button */}
         <h5 className="card-title">{title}</h5>
-        <img src={avatar} className="card-img-top" alt="Avatar" />
+        <img src={image} className="card-img-top" alt="Avatar" />
         <div className="button-container">
           <a href="#" className="btn btn-primary" style={{ width: '100%' }}>Buy {money}</a>
         </div>
